ea18d77655ba9e8c4647518917e3c21b1be19483
thasup <thanachonfirst@hotmail.com>
2022-02-14 22:32:29 +0700

    Fix images loading issue with isotope script
    Images finished loading after the isotope script has started. It make images stack upon the first time the website was loaded up.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 // ------ Hero section typed effect ------
 const typed = $(".typed");
 if (typed) {
-    let typed_strings = typed.attr("data-typed-items");
-    typed_strings = typed_strings.split(",");
-    new Typed(".typed", {
-        strings: typed_strings,
-        loop: true,
-        typeSpeed: 100,
-        backSpeed: 50,
-        backDelay: 2000,
-    });
+  let typed_strings = typed.attr("data-typed-items");
+  typed_strings = typed_strings.split(",");
+  new Typed(".typed", {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
+  });
 }
 
 // ------ Add btn-started functionality ------
 const startedBtn = $(".btn-started");
 
 function goToNavbar(element) {
-    element.on("click", (e) => {
-        e.preventDefault();
-        window.scrollTo({
-            top: $("#navbar").offset().top,
-            behavior: "smooth",
-        });
+  element.on("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: $("#navbar").offset().top,
+      behavior: "smooth",
     });
+  });
 }
 
 goToNavbar(startedBtn);
@@ -32,23 +32,23 @@ const backtotop = $(".back-to-top");
 const websiteLogo = $(".navbar-brand");
 
 function backToTop(element) {
-    element.on("click", (e) => {
-        e.preventDefault();
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-        });
+  element.on("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
     });
+  });
 
-    function toggleBacktotop() {
-        if (window.scrollY > 150) {
-            backtotop.addClass("active");
-        } else {
-            backtotop.removeClass("active");
-        }
+  function toggleBacktotop() {
+    if (window.scrollY > 150) {
+      backtotop.addClass("active");
+    } else {
+      backtotop.removeClass("active");
     }
+  }
 
-    $(window).on("scroll", toggleBacktotop);
+  $(window).on("scroll", toggleBacktotop);
 }
 
 backToTop(backtotop);
@@ -56,31 +56,31 @@ backToTop(websiteLogo);
 
 // ------ Add active-section in navbar when scroll through that section ------
 function addActiveSection() {
-    const sections = $("section");
-    sections.each(function (i, section) {
-        let $section = $(section);
-        const currentNavLink = $(`#${section.id}-link`);
-        let position = window.scrollY + 300;
-        //debug
-        // console.log("section", section);
-        // console.log("$section", $section);
-        // console.log("position", position);
-        // console.log("currentNavLink", currentNavLink[0].id);
-        // console.log("offsetTop", section.offsetTop);
-        // console.log("offsetHeight", section.offsetHeight);
-        // console.log(
-        //     "section.offsetTop + section.offsetHeight",
-        //     section.offsetTop + section.offsetHeight
-        // );
-        if (
-            position >= $section.offset().top &&
-            position <= $section.offset().top + $section.outerHeight()
-        ) {
-            currentNavLink.addClass("active-section");
-        } else {
-            currentNavLink.removeClass("active-section");
-        }
-    });
+  const sections = $("section");
+  sections.each(function (i, section) {
+    let $section = $(section);
+    const currentNavLink = $(`#${section.id}-link`);
+    let position = window.scrollY + 300;
+    //debug
+    // console.log("section", section);
+    // console.log("$section", $section);
+    // console.log("position", position);
+    // console.log("currentNavLink", currentNavLink[0].id);
+    // console.log("offsetTop", section.offsetTop);
+    // console.log("offsetHeight", section.offsetHeight);
+    // console.log(
+    //     "section.offsetTop + section.offsetHeight",
+    //     section.offsetTop + section.offsetHeight
+    // );
+    if (
+      position >= $section.offset().top &&
+      position <= $section.offset().top + $section.outerHeight()
+    ) {
+      currentNavLink.addClass("active-section");
+    } else {
+      currentNavLink.removeClass("active-section");
+    }
+  });
 }
 $(window).on("scroll", addActiveSection);
 
@@ -88,43 +88,46 @@ $(window).on("scroll", addActiveSection);
 const navLinks = $(".sections-name");
 
 function scrollToSection(element) {
-    $("html, body").animate(
-        {
-            scrollTop:
-                $(`#${element}`).offset().top - $("header").innerHeight(),
-        },
-        1
-    );
+  $("html, body").animate(
+    {
+      scrollTop: $(`#${element}`).offset().top - $("header").innerHeight(),
+    },
+    1
+  );
 }
 
 function addLinks(sections) {
-    for (section of sections) {
-        const sectionName = section.id.slice(0, -5);
+  for (section of sections) {
+    const sectionName = section.id.slice(0, -5);
 
-        $(section).click(function (e) {
-            e.preventDefault();
-            scrollToSection(sectionName);
-        });
-    }
+    $(section).click(function (e) {
+      e.preventDefault();
+      scrollToSection(sectionName);
+    });
+  }
 }
 
 $(document).ready(addLinks(navLinks));
 
 // ------ Filter portfolio with Isotope script ------
-const iso = $(".portfolio-container").isotope({
+$(window).on("load", function () {
+  const iso = $(".portfolio-container").isotope({
     // options...
     itemSelector: ".portfolio-item",
     layoutMode: "masonry",
+  });
+  // fix images loading time issue before isotope script run
+  $(".portfolio-container").css({ opacity: 1 });
 });
 
 // filter functions object
 const filterFns = {
-    // show if contain with react tag
-    react: function () {
-        console.log(this);
-        let name = $(this).find(".tech-tag").children().text();
-        return name.match(/react/);
-    },
+  // show if contain with react tag
+  react: function () {
+    console.log(this);
+    let name = $(this).find(".tech-tag").children().text();
+    return name.match(/react/);
+  },
 };
 
 // defined variable to hold value
@@ -133,33 +136,32 @@ var secondaryFilterValue = "";
 
 // bind filter button click
 $(".portfolio-filters").on("click", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    if (event.target.parentElement.id === "primary") {
-        // primary filters
-        primaryFilterValue = event.target.getAttribute("data-filter");
-    } else if (event.target.parentElement.id === "secondary") {
-        // secondary filters
-        secondaryFilterValue = event.target.getAttribute("data-filter");
-    }
+  if (event.target.parentElement.id === "primary") {
+    // primary filters
+    primaryFilterValue = event.target.getAttribute("data-filter");
+  } else if (event.target.parentElement.id === "secondary") {
+    // secondary filters
+    secondaryFilterValue = event.target.getAttribute("data-filter");
+  }
 
-    let filterValue;
-    filterValue =
-        filterFns[filterValue] ||
-        `${primaryFilterValue}${secondaryFilterValue}`;
+  let filterValue;
+  filterValue =
+    filterFns[filterValue] || `${primaryFilterValue}${secondaryFilterValue}`;
 
-    $(".portfolio-container").isotope({ filter: filterValue });
+  $(".portfolio-container").isotope({ filter: filterValue });
 });
 
 // change filter-active class on buttons
 function activeFilters(filter) {
-    filter.each(function (i, filter) {
-        var $filter = $(filter);
-        $filter.on("click", "li", function () {
-            $filter.find(".filter-active").removeClass("filter-active");
-            $(this).addClass("filter-active");
-        });
+  filter.each(function (i, filter) {
+    var $filter = $(filter);
+    $filter.on("click", "li", function () {
+      $filter.find(".filter-active").removeClass("filter-active");
+      $(this).addClass("filter-active");
     });
+  });
 }
 
 activeFilters($(".primary-filters"));
@@ -167,13 +169,13 @@ activeFilters($(".secondary-filters"));
 
 // ------ Add more filters ------
 function moreFilters() {
-    const dropdown = $(".filters-more");
-    dropdown.click((e) => {
-        e.preventDefault();
-        $(".filters-section").toggleClass("show-filters");
-        $(".secondary-filters").toggleClass("active-filters");
-        $(".filters-more").toggleClass("filp");
-    });
+  const dropdown = $(".filters-more");
+  dropdown.click((e) => {
+    e.preventDefault();
+    $(".filters-section").toggleClass("show-filters");
+    $(".secondary-filters").toggleClass("active-filters");
+    $(".filters-more").toggleClass("filp");
+  });
 }
 
 moreFilters();
